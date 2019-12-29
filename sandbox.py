17d0cd22d481a1bcafa5d974d82de0bbd486a319
Shawn Ross <shawn@fedarch.org>
2019-12-29 22:44:17 +0100

    Improve comments in code
    Comments added to code to explain each step, so that I can remember what 
    I am doing tomorrow.

--- sandbox.py
+++ sandbox.py
@@ -5,13 +5,19 @@
 import numpy
 import random
 
+# Learn basics of monte carlo simulations, starting with gambling with dice
+# https://pythonprogramming.net/monte-carlo-simulator-python/
+
+# Create dice
+
 def rollDice():
     roll = random.randint(1,100)
     return roll
+
+# Roll dice 100 times and print all rolls
 
 x = 0
 while x < 100:
     result =rollDice()
     print(result)
     x=x+1
-    
