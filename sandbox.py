d8ff8c7372b43449f198f2b104cf90d32a199ccc
Shawn Ross <shawn@fedarch.org>
2019-12-30 17:25:52 +0100

    Sanity check
    Reviewed code and experimented with different values to ensure that as 
    the number of rounds increases, bettors are going broke.

--- sandbox.py
+++ sandbox.py
@@ -30,13 +30,13 @@ def rollDice():
     roll = random.randint(1,100)
 
     if roll == 100:
-        print(roll, 'loss')
+        #print(roll, 'roll was 100, loss')
         return False
     elif roll <= 50:
-        print(roll, 'loss')
+        #print(roll, 'roll was <50, loss')
         return False
-    elif 100 > roll >= 51:
-        print(roll,'win')
+    elif 100 > roll > 50:
+        #print(roll,'roll was 51-99, win')
         return True
 
 # Create a simple bettor, betting the same amount each time
@@ -54,6 +54,20 @@ def simple_bettor(funds, initial_wager, wager_count):
             value = value-wager
 
         currentWager = currentWager+1
+
+# Exit out if funds ever fall to zero
+
+    if value <= 0:
+        value = 'Broke!'
         print('Funds:', value)
 
-simple_bettor(10000,100,100)
+# If bettor survives all rounds, print fund balance
+
+    print('Funds:', value)
+
+# Now run lots of wagers
+
+x = 0
+while x < 100:
+    simple_bettor(10000,100,100)
+    x += 1
