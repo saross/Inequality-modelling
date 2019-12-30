3907fddf4c7fff34c3ae3946ce1d776a89a9fe09
Shawn Ross <shawn@fedarch.org>
2019-12-30 16:58:56 +0100

    Create simple bettor
    Dice are working. Now we creat a simple bettor who has 10,000 units and 
    bets 100 each time, for 100 rounds.

--- sandbox.py
+++ sandbox.py
@@ -1,23 +1,59 @@
+###########################################################
 # Sandbox for experimenting with monte carlo simulations
-#
+###########################################################
 #
 # Load libraries
+
 import numpy
 import random
 
 # Learn basics of monte carlo simulations, starting with gambling with dice
 # https://pythonprogramming.net/monte-carlo-simulator-python/
 
-# Create dice
+# # Create dice
+#
+# def rollDice():
+#     roll = random.randint(1,100)
+#     return roll
+#
+# # Roll dice 100 times and print all rolls
+#
+# x = 0
+# while x < 100:
+#     result =rollDice()
+#     print(result)
+#     x = x+1
+
+# Change to simple win-loss
 
 def rollDice():
     roll = random.randint(1,100)
-    return roll
 
-# Roll dice 100 times and print all rolls
+    if roll == 100:
+        print(roll, 'loss')
+        return False
+    elif roll <= 50:
+        print(roll, 'loss')
+        return False
+    elif 100 > roll >= 51:
+        print(roll,'win')
+        return True
 
-x = 0
-while x < 100:
-    result =rollDice()
-    print(result)
-    x=x+1
+# Create a simple bettor, betting the same amount each time
+
+def simple_bettor(funds, initial_wager, wager_count):
+    value = funds
+    wager = initial_wager
+
+    currentWager = 0
+
+    while currentWager < wager_count:
+        if rollDice():
+            value = value+wager
+        else:
+            value = value-wager
+
+        currentWager = currentWager+1
+        print('Funds:', value)
+
+simple_bettor(10000,100,100)
