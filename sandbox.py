ac16d45e7eecd0dd02e542a3df8fb4272b3c34fa
Shawn Ross <shawn@fedarch.org>
2020-01-03 10:26:05 -0800

    Implement graphing of results
    Uisng the next lesson in the tutorial, implement graphing of results using matplotlib.

--- sandbox.py
+++ sandbox.py
@@ -4,8 +4,10 @@
 #
 # Load libraries
 
-import numpy
 import random
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy
 
 # Learn basics of monte carlo simulations, starting with gambling with dice
 # https://pythonprogramming.net/monte-carlo-simulator-python/
@@ -45,29 +47,51 @@ def simple_bettor(funds, initial_wager, wager_count):
     value = funds
     wager = initial_wager
 
-    currentWager = 1
+    # make wager the x variable
+
+    xW = []
+
+    # make value (funds) the Y variable
+
+    yV = []
+
+
+    currentWager = 1 # start at 1 and not 0 for first wager
 
     while currentWager <= wager_count:
         if rollDice():
             value = value+wager
+            # append current wager number and new value (funds balance) if bet is successful
+            wX.append(currentWager)
+            vY.append(value)
+
         else:
             value = value-wager
+            # append current wager number and new value (funds blance) if bet is unsuccessful
+            wX.append(currentWager)
+            vY.append(value)
 
         currentWager = currentWager+1
 
 # Exit out if funds ever fall to zero
 
-    if value <= 0:
-        value = 'Broke!'
-        print('Funds:', value)
+    # if value <= 0:
+    #     value = 'Broke!'
+    #     print('Funds:', value)
 
 # If bettor survives all rounds, print fund balance
 
-    print('Funds:', value)
+    # print('Funds:', value)
+
+    plt.plot(wX,vY)
 
 # Now run lots of wagers
 
 x = 0
 while x < 100:
-    simple_bettor(10000,100,100)
+    simple_bettor(10000,100,50)
     x += 1
+
+plt.ylabel('Account Value')
+plt.xlabel('Wager Count')
+plt.show()
