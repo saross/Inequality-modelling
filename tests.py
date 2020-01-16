1cd8fb1f59fe30a376b9e9861537a4d93851e31c
Shawn Ross <shawn@fedarch.org>
2020-01-16 23:25:17 +0200

    Build array from scratch
    Began building array from scratch to understand how it works.

--- tests.py
+++ tests.py
@@ -4,16 +4,10 @@
 #
 ##############################################
 
-import random
+import numpy as np
 
-def rollDice():
-    roll = random.randint(-100,100)
-    return roll
+y = np.empty([2])
 
-# Now, just to test our dice, let's roll the dice 100 times. 
+x = np.append(y, [2, 3])
 
-x = 0
-while x < 100:
-    result = rollDice()
-    print(result)
-    x += 1
+print(x)
